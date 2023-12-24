be6e43c0d92a1c7aeaa04d601500d56dd1a2728d
SeanBrownn <108001967+SeanBrownn@users.noreply.github.com>
2023-12-23 22:33:33 -0500

    teams functions work correctly

--- teams.py
+++ teams.py
@@ -7,68 +7,41 @@ class teams:
 
     current_year=datetime.datetime.now().year
 
-    # default behaviors:
-    # start_season: starts from current year
-    # end_season: if not provided, only returns data from start_season
-    # league: returns data from all leagues
-    # ind: returns one row per team per year (doesn't aggregate)
+    # ind: default is returning one row per team per year (doesn't aggregate)
     @staticmethod
     def get_team_batting(start_season: int = current_year, end_season: int = None, league: str = 'all', ind: int = 1):
-        return pybaseball.team_batting(start_season, end_season, league, ind).to_dict(orient='records')
+        return pybaseball.team_batting(start_season, end_season, league, ind).to_json(orient='records')
 
-    # default behaviors:
-    # team: no default behavior, team must be provided
-    # start_season: starts from current year
-    # end_season: if not provided, only returns data from start_season
     @staticmethod
     def get_team_batting_bref(team: str, start_season: int = current_year, end_season: int = None):
-        return pybaseball.team_batting_bref(team, start_season, end_season).to_dict(orient='records')
+        return pybaseball.team_batting_bref(team, start_season, end_season).to_json(orient='records')
 
-    # default behaviors:
-    # start_season: starts from current year
-    # end_season: if not provided, only returns data from start_season
-    # league: returns data from all leagues
-    # ind: returns one row per team per year (doesn't aggregate)
+    # ind: default is returning one row per team per year (doesn't aggregate)
     @staticmethod
     def get_team_fielding(start_season: int = current_year, end_season: int = None, league: str = 'all', ind: int = 1):
-        return pybaseball.team_fielding(start_season, end_season, league, ind).to_dict(orient='records')
+        return pybaseball.team_fielding(start_season, end_season, league, ind).to_json(orient='records')
 
-    # default behaviors:
-    # team: no default behavior, team must be provided
-    # start_season: starts from current year
-    # end_season: if not provided, only returns data from start_season
+    # The column names from the dataframe outputted by pybaseball.team_fielding_bref were not unique. So, I had to use
+    # orient='values.'
     @staticmethod
     def get_team_fielding_bref(team: str, start_season: int = current_year, end_season: int = None):
-        return pybaseball.team_fielding_bref(team, start_season, end_season).to_dict(orient='records')
+        return pybaseball.team_fielding_bref(team, start_season, end_season).to_json(orient='values')
 
-    # default behaviors:
-    # team: no default behavior, team must be provided
-    # season: gets logs from current year
-    # log_type: gets batting logs
+    # log_type: default is getting batting logs
     @staticmethod
     def get_team_game_logs(team: str, season: int = current_year, log_type: str = "batting"):
-        return pybaseball.team_game_logs(season, team, log_type)
+        return pybaseball.team_game_logs(season, team, log_type).to_json(orient='records')
 
-    # default behaviors:
-    # start_season: starts from current year
-    # end_season: if not provided, only returns data from start_season
-    # league: returns data from all leagues
-    # ind: returns one row per team per year (doesn't aggregate)
+    # ind: defaults to returning one row per team per year (doesn't aggregate)
     @staticmethod
     def get_team_pitching(start_season: int = current_year, end_season: int = None, league: str = 'all', ind: int = 1):
-        return pybaseball.team_pitching(start_season, end_season, league, ind)
+        return pybaseball.team_pitching(start_season, end_season, league, ind).to_json(orient='records')
 
-    # default behaviors:
-    # team: no default behavior, team must be provided
-    # start_season: starts from current year
-    # end_season: if not provided, only returns data from start_season
     @staticmethod
     def get_team_pitching_bref(team: str, start_season: int = current_year, end_season: int = None):
-        return pybaseball.team_pitching_bref(team, start_season, end_season)
+        return pybaseball.team_pitching_bref(team, start_season, end_season).to_json(orient='records')
 
-    # default behaviors:
-    # season: returns all seasons if not provided
-    # league: returns data from all leagues
+    # returns a mapping (empty) dataframe
     @staticmethod
     def get_team_ids(season: int = None, league: str = None):
-        return pybaseball.team_ids(season, league)
+        return pybaseball.team_ids(season, league).to_json(orient='records')
